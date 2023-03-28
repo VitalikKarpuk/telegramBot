5b9a0aee614eb7ab9c070755ec634e6c2af62e34
Vitali Karpuk <VKarpuk@inspirebrands.com>
2023-03-28 15:23:31 +0300

    fix

--- node.js
+++ node.js
@@ -1,13 +1,40 @@
 const TelegramBot = require("node-telegram-bot-api");
-const debug = require("./helpers");
-const fs = require("fs");
+// const debug = require("./helpers");
+// const fs = require("fs");
 const TOKEN = "5553791482:AAEZ367kZW9al_Ro6yTnsCOjlRDwl6jSmaM";
 const bot = new TelegramBot(TOKEN, {
   polling: true,
 });
 
-bot.onText(/\/pic2/, (msg) => {
-  bot.sendPhoto(msg.chat.id, "/cat.png", {
-    caption: "1111",
+
+const inline_keyboard = [
+  [
+    {
+      text: "Forward",
+      callback_data: "forward",
+    },
+    {
+      text: "Reply",
+      callback_data: "reply",
+    },
+  ],
+  [
+    {
+      text: "Edit",
+      callback_data: "edit",
+    },
+    {
+      text: "Delete",
+      callback_data: "delete",
+    },
+  ],
+];
+
+bot.onText(/\/start/, (msg, [source, match]) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Keyboard", {
+    reply_markup: {
+      inline_keyboard,
+    },
   });
 });
