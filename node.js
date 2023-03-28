aa7723d815a0fd2c610535b0c22d22e2ac9e6b8d
Vitali Karpuk <VKarpuk@inspirebrands.com>
2023-03-28 16:15:33 +0300

    fix

--- node.js
+++ node.js
@@ -1,11 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api");
-// const debug = require("./helpers");
-// const fs = require("fs");
 const TOKEN = "5553791482:AAEZ367kZW9al_Ro6yTnsCOjlRDwl6jSmaM";
 const bot = new TelegramBot(TOKEN, {
   polling: true,
 });
-
 
 const inline_keyboard = [
   [
