fd98fd54cd442ac414a8424f48f943dc9829b562
Vitali Karpuk <VKarpuk@inspirebrands.com>
2023-03-29 17:54:08 +0300

    fix

--- initApp.js
+++ initApp.js
@@ -1,6 +1,8 @@
 const dashTelegramBot = require("./dashaTelegramBot");
+const testingVK2022Bot = require("./Vitali/testingVK2022Bot");
 const initApp = () => {
   dashTelegramBot();
+  testingVK2022Bot();
 };
 
 module.exports = initApp;
