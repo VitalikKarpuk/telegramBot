af4289034d012d9c31b69de2a0b3b7e5c82d1bec
Vitali Karpuk <VKarpuk@inspirebrands.com>
2023-03-29 17:49:36 +0300

    fix

---
+++ Vitali/testingVK2022Bot.js
@@ -0,0 +1,42 @@
+const testingVK2022Bot = () => {
+  console.log("testingVK2022Bot starting");
+  const TelegramBot = require("node-telegram-bot-api");
+  const TOKEN = "5553791482:AAEZ367kZW9al_Ro6yTnsCOjlRDwl6jSmaM";
+  const bot = new TelegramBot(TOKEN, {
+    polling: true,
+  });
+
+  const inline_keyboard = [
+    [
+      {
+        text: "Forward",
+        callback_data: "forward",
+      },
+      {
+        text: "Reply",
+        callback_data: "reply",
+      },
+    ],
+    [
+      {
+        text: "Edit",
+        callback_data: "edit",
+      },
+      {
+        text: "Delejhgjbjbte",
+        callback_data: "delete",
+      },
+    ],
+  ];
+
+  bot.onText(/\/start/, (msg, [source, match]) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "testingVK2022Bot", {
+      reply_markup: {
+        inline_keyboard,
+      },
+    });
+  });
+};
+
+module.exports = testingVK2022Bot;
